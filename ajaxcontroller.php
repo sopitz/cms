f9fa92a5cc2c712ec63eeac49971caff4d519342
Simon Opitz <simon@opitzfamily.de>
2013-02-04 22:40:52 +0100

    format code

--- ajaxcontroller.php
+++ ajaxcontroller.php
@@ -1,9 +1,8 @@
 <?php
-$controller = $_POST['controller'];
-$action = $_POST['action'];
-$args = $_POST['args'];
-
-require("apps/".$controller."/".$controller.".php");
-$controller::$action($args);
-
+	$controller = $_POST['controller'];
+	$action = $_POST['action'];
+	$args = $_POST['args'];
+	
+	require("apps/".$controller."/".$controller.".php");
+	$controller::$action($args);
 ?>
--- models/error.php
+++ models/error.php
@@ -2,7 +2,7 @@
 class ErrorModel extends BaseModel
 {    
     public function pageNotFound() {
-        $this->viewModel->set("pageTitle","Fehler 404 <br /> Seite nicht gefunden");
+        $this->viewModel->set("pageTitle", "Fehler 404 <br /> Seite nicht gefunden");
         return $this->viewModel;
     }
 }
