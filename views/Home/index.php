a920f38cfe45d27f80e86b68f9abb7d35eb53776
Simon Opitz <simon@opitzfamily.de>
2013-02-06 17:00:32 +0100

    implement mercury and create example

--- views/Home/index.php
+++ views/Home/index.php
@@ -1,4 +1,22 @@
+
+
+
 <?php 
 	require_once('apps/login/login.php');
 	Login::getView("form");
 ?>
+
+<div class="editable">im an editable div</div>
+<script type="text/javascript">
+$('.editable').click(function(e) {
+	Mercury.trigger('toggle:interface');
+	$('.editable').attr('contenteditable', 'true');
+	$('.editable').focus();
+	
+	$('.editable').off('click');
+});
+</script>
+
+
+
+<script src="lib/mercury/javascripts/mercury_loader.js?pack=bundled&visible=false" type="text/javascript"></script>
