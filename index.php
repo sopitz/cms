76cdfe232db0a334390cf0987d4d3b974ac8d597
Simon Opitz <simon@iss-opitz.de>
2013-03-06 23:11:41 +0100

    write apache_request_headers() as its not provided by stratos apache

--- index.php
+++ index.php
@@ -13,6 +13,29 @@ require("classes/loader.php");
 
 
 
+
+	function apache_request_headers() {
+		$arh = array();
+		$rx_http = '/\AHTTP_/';
+		foreach($_SERVER as $key => $val) {
+			if( preg_match($rx_http, $key) ) {
+				$arh_key = preg_replace($rx_http, '', $key);
+				$rx_matches = array();
+				// do some nasty string manipulations to restore the original letter case
+				// this should work in most cases
+				$rx_matches = explode('_', $arh_key);
+				if( count($rx_matches) > 0 and strlen($arh_key) > 2 ) {
+					foreach($rx_matches as $ak_key => $ak_val) $rx_matches[$ak_key] = ucfirst($ak_val);
+					$arh_key = implode('-', $rx_matches);
+				}
+				$arh[$arh_key] = $val;
+			}
+		}
+		return( $arh );
+	}
+
+
+
 $urlValues = $_GET;
 $controllerName;
 $action;
@@ -51,5 +74,6 @@ if ((strpos($headers['If-None-Match'], "$eTag")) && (gmstrftime("%a, %d %b %Y %T
 	$controller = $loader->createController();
 	$controller->executeAction();
 }
+
 
 ?>
