a8cf8fd1b9b6ca258c9358d5c9193306707dd3b2
Simon Opitz <simon@iss-opitz.de>
2013-03-06 20:52:10 +0100

    debugged caching
    previous version would not find changed parts withing the template…only
    images, css, js etc. Now its handling the template-parts as well.

--- index.php
+++ index.php
@@ -4,11 +4,33 @@ if (isset($_COOKIE['user'])) {
 	setcookie("user", $value, time()+3600);
 }
 
+include("classes/error.php");
+require("classes/basecontroller.php");
+require("classes/basemodel.php");
+require("classes/view.php");
+require("classes/viewmodel.php");
+require("classes/loader.php");
+
+
+
+$urlValues = $_GET;
+$controllerName;
+$action;
+if ($urlValues['controller'] == "") {
+	$controllerName = "home";
+} else {
+	$controllerName = strtolower($urlValues['controller']);
+}
+if ($urlValues['action'] == "") {
+	$action = "index";
+} else {
+	$action = $urlValues['action'];
+}
+$file = "./views/".$controllerName."/".$action.".php";
 $headers = apache_request_headers();
 list(,,,,,,,,,$lastModified) = stat($file);
-$eTag = "ci-".dechex(crc32($file.$lastModified));
-if ((strpos($headers['If-None-Match'], "$eTag")) &&
-		(gmstrftime("%a, %d %b %Y %T %Z", $lastModified) == $headers['If-Modified-Since'])) {
+$eTag = "te-".dechex(crc32($file.$lastModified));
+if ((strpos($headers['If-None-Match'], "$eTag")) && (gmstrftime("%a, %d %b %Y %T %Z", $lastModified) == $headers['If-Modified-Since'])) {
 	header('HTTP/1.1 304 Not Modified');
 	header('Cache-Control: private');
 	header("Pragma: ");
@@ -17,12 +39,7 @@ if ((strpos($headers['If-None-Match'], "$eTag")) &&
 	header('ETag: "'.$eTag.'"');
 	exit;
 } else {
-	include("classes/error.php");
-	require("classes/basecontroller.php");  
-	require("classes/basemodel.php");
-	require("classes/view.php");
-	require("classes/viewmodel.php");
-	require("classes/loader.php");
+	
 
 	header('Cache-Control: private');
 	header('Pragma: ');
